fdba0cb4cdb820bc525bc16ba7f9195aa897a948
vctry4reel <johnsonvictory911@gmail.com>
2024-01-31 10:51:39 +0100

    Login and Sign up

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
+import Registration from './components/Registration'
 function App() {
   return (
-    <div class="w-[450px] h-[450px] bg-skin-primary">
-      <h1 class="text-color-white">Happy coding</h1>
-    </div>
+    <Registration/>
   );
 }
 
