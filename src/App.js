29c166298231ba2e50151fe2537d0b249cab19fe
Jutelabs <julietech3@gmail.com>
2024-02-03 04:12:51 +0100

    have updated my design

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 // src/App.js
 import React from 'react';
 import OnboardingCarousel from './components/OnboardingCarousel';
-import './App.css';
 import './index.css';
-
+import './App.css';
 function App() {
   return (
     <div className="app">
@@ -13,19 +12,3 @@ function App() {
 }
 
 export default App;
-
-
-// src/index.js or src/App.js
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import App from './App';
-//  // Import your global styles here
-
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
-
-// export default App;
