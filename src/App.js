714fe2c139515ef4fbe5f6915327bf1a69c99568
sustainspicy <isrealspicy@gmail.com>
2024-02-06 16:47:42 +0100

    conflict resolved

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import { Route, Routes } from 'react-router-dom';
 import Registration from './components/Registration';
 import LandingPage from './pages/LandingPage';
 import NavBar from './components/NavBar';
+import Auth from './pages/Auth';
 function App() {
   return (
     <>
       {/* <NavBar/> */}
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route path="/registration" element={<Registration />} />
+        <Route path="/auth" element={<Auth />} />
       </Routes>
     </>
   );
