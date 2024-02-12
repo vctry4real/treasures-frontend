2ceebaf6ab8ebbc06ace82d975dfabdc127b5a0d
leo417 <leosab_2@hotmail.com>
2024-02-12 16:48:59 +0100

    fixed uniform formats and made docdata

--- src/components/OtpForm.jsx
+++ src/components/OtpForm.jsx
@@ -217,76 +217,8 @@ const OtpForm = ({ setAuthType }) => {
           Continue
         </button>
       </div>
-
-      {/* <h1 className="mb-1 text-gray-700 text-xl font-bold tracking-wide p-8 text-center">Forgot your password</h1>
-      <p className="pb-2">
-              Enter your registered email address
-            </p>
-      <div className="mb-4">
-        <label
-          className="block text-gray-700 text-sm font-bold mb-2"
-          htmlFor="email"
-        >
-          Email:
-        </label>
-        <input
-          className="w-full border rounded-md py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"
-          type="email"
-          name="email"
-          value={formData.email}
-          onChange={handleChange}
-          autoComplete="email"
-          required
-        />
-      </div>
-
-      <div className="text-center">
-        <button
-          type="submit"
-          className="w-full mt-4 bg-primary hover:bg-lightGreen text-white p-2 rounded "
-        >
-          Login
-        </button>
-      </div> */}
     </motion.form>
   );
-
-  // const [showPassword, setShowPassword] = useState(false);
-
-  // const handleChange = (e) => {
-  //   const sanitizedInput = DOMPurify.sanitize(e.target.value);
-  //   setFormData((prev) => ({
-  //     ...prev,
-  //     [e.target.name]: sanitizedInput,
-  //   }));
-  // };
-
-  // const handleSubmit = async (e) => {
-  //   e.preventDefault();
-  //   const { email } = formData;
-  //   if (!email) {
-  //     showAlert({ text: 'Please fill in registered email', type: 'danger' });
-  //     return;
-  //   }
-
-  //   // Temp stopping the email verification
-  //   return
-  //   try {
-  //     const { status, data } = await PublicApi.post('/auth/signin', formData, {
-  //       headers: {
-  //         'Content-Type': 'application/json',
-  //       },
-  //     });
-  //     if (status === 200) {
-  //       console.log(data);
-  //       showAlert({ text: 'User authenticated', type: 'success' });
-  //       navigate('/Reset');
-  //     }
-  //   } catch (error) {
-  //     console.log(error.response.data.msg);
-  //     showAlert({ text: error.response.data.msg, type: 'danger' });
-  //   }
-  // };
 };
 
 export default OtpForm;
